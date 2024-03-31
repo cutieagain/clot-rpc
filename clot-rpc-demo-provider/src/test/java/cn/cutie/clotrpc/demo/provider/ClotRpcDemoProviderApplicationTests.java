d5061e9f4ebce1495df3efc57138fcd8e8eff8fa
cutie <330449857@qq.com>
2024-03-31 23:20:08 +0800

    1.单元测试内嵌zk

--- clot-rpc-demo-provider/src/test/java/cn/cutie/clotrpc/demo/provider/ClotRpcDemoProviderApplicationTests.java
+++ clot-rpc-demo-provider/src/test/java/cn/cutie/clotrpc/demo/provider/ClotRpcDemoProviderApplicationTests.java
@@ -1,13 +1,30 @@
 package cn.cutie.clotrpc.demo.provider;
 
+import cn.cutie.clotrpc.core.test.TestZKServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {ClotRpcDemoProviderApplication.class})
 class ClotRpcDemoProviderApplicationTests {
+
+    static TestZKServer zkServer = new TestZKServer();
+
+    @BeforeAll
+    static void init(){
+        zkServer.start();
+    }
 
     @Test
     void contextLoads() {
+        System.out.println("===> ClotRpcDemoProviderApplicationTests contextLoads.");
+    }
+
+    @AfterAll
+    static void destroy(){
+        zkServer.stop();
     }
 
 }
