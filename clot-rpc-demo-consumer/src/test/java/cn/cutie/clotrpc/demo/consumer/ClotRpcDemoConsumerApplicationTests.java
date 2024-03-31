f1bb9be8191f17883a72dc64c67e63fc79727251
cutie <330449857@qq.com>
2024-03-31 23:06:03 +0800

    1.单元测试内嵌zk

--- clot-rpc-demo-consumer/src/test/java/cn/cutie/clotrpc/demo/consumer/ClotRpcDemoConsumerApplicationTests.java
+++ clot-rpc-demo-consumer/src/test/java/cn/cutie/clotrpc/demo/consumer/ClotRpcDemoConsumerApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.cutie.clotrpc.demo.consumer;
 
+import cn.cutie.clotrpc.core.test.TestZKServer;
 import cn.cutie.clotrpc.demo.provider.ClotRpcDemoProviderApplication;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,13 +13,18 @@ import org.springframework.context.ApplicationContext;
 class ClotRpcDemoConsumerApplicationTests {
     static ApplicationContext applicationContext;
 
+    static TestZKServer zkServer = new TestZKServer();
+
     /**
      * 执行启动provider的逻辑
      */
     @BeforeAll
     static void init(){
+        zkServer.start();
         applicationContext = SpringApplication.run(ClotRpcDemoProviderApplication.class,
-                "--server.port=8084", "--logging.level.cn.cutie=debug");
+                "--server.port=8084",
+                "--logging.level.cn.cutie=debug",
+                "--clotrpc.zkServer=localhost:2182");
     }
 
     @Test
@@ -32,6 +38,7 @@ class ClotRpcDemoConsumerApplicationTests {
     @AfterAll
     static void destroy(){
         SpringApplication.exit(applicationContext, () -> 1);
+        zkServer.stop();
     }
 
 }
