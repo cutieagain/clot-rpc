bf57b8fd9006a23ff3d504a658bf6176e1c3c938
cutie <330449857@qq.com>
2024-04-08 22:29:54 +0800

    concumer测试启动两个provider，并完善启动参数

--- clot-rpc-demo-consumer/src/test/java/cn/cutie/clotrpc/demo/consumer/ClotRpcDemoConsumerApplicationTests.java
+++ clot-rpc-demo-consumer/src/test/java/cn/cutie/clotrpc/demo/consumer/ClotRpcDemoConsumerApplicationTests.java
@@ -11,7 +11,8 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootTest(classes = {ClotRpcDemoConsumerApplication.class})
 class ClotRpcDemoConsumerApplicationTests {
-    static ApplicationContext applicationContext;
+    static ApplicationContext context1;
+    static ApplicationContext context2;
 
     static TestZKServer zkServer = new TestZKServer();
 
@@ -20,11 +21,39 @@ class ClotRpcDemoConsumerApplicationTests {
      */
     @BeforeAll
     static void init(){
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" =============     ZK2182    ========== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
         zkServer.start();
-        applicationContext = SpringApplication.run(ClotRpcDemoProviderApplication.class,
-                "--server.port=8084",
-                "--logging.level.cn.cutie=debug",
-                "--clotrpc.zkServer=localhost:2182");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" =============      P8094    ========== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        context1 = SpringApplication.run(ClotRpcDemoProviderApplication.class,
+                        "--server.port=8094",
+                        "--clotrpc.zk.server=localhost:2182",
+                        "--clotrpc.app.env=test",
+                        "--logging.level.cn.cutie.clotrpc=info",
+                        "--clotrpc.provider.metas.dc=bj",
+                        "--clotrpc.provider.metas.gray=false",
+                        "--clotrpc.provider.metas.unit=B001");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" =============      P8095    ========== ");
+        System.out.println(" ====================================== ");
+        System.out.println(" ====================================== ");
+        context2 = SpringApplication.run(ClotRpcDemoProviderApplication.class,
+                "--server.port=8095",
+                "--kkrpc.zk.server=localhost:2182",
+                "--kkrpc.app.env=test",
+                "--logging.level.cn.cutie.clotrpc=info",
+                "--kkrpc.provider.metas.dc=bj",
+                "--kkrpc.provider.metas.gray=false",
+                "--kkrpc.provider.metas.unit=B002"
+        );
     }
 
     @Test
@@ -37,7 +66,8 @@ class ClotRpcDemoConsumerApplicationTests {
      */
     @AfterAll
     static void destroy(){
-        SpringApplication.exit(applicationContext, () -> 1);
+        SpringApplication.exit(context1, () -> 1);
+        SpringApplication.exit(context2, () -> 1);
         zkServer.stop();
     }
 
